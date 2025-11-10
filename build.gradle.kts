d807fb524333fb8380078fbe0b90b3f7ff1e50cd
moazelgebaly25 <moaz.elgebaly.25@gmail.com>
2025-11-10 23:37:45 +0200

    Initial commit: Trailerly - Movie browsing Android app

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Project-level build.gradle.kts
+plugins {
+    id("com.android.application") version "8.13.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
+}
